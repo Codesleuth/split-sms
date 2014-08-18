a941f5c7895e65c51fc304b781811f618c74b197
David Wood <david.p.wood@gmail.com>
2014-08-18 22:29:26 +0100

    Adds the form feed character to the GSM detector list.

--- test/charactersetdetector.test.js
+++ test/charactersetdetector.test.js
@@ -6,7 +6,7 @@ describe('Message Character Set Detector', function () {
   describe('Detecting a message of all the known GSM characters', function () {
 
     it('should return GSM', function () {
-      var message = '@Δ\x200¡P¿p£_!1AQaq$Φ"2BRbr¥Γ#3CScsèΛ¤4DTdtéΩ%5EUeuùΠ&6FVfvìΨ\'7GWgwòΣ(8HXhxÇΘ)9IYiy\nΞ*:JZjzØ+;KÄkäøÆ,<LÖlö\ræ-=MÑmñÅß.>NÜnüåÉ/?O§oà|^€{}[~]\\';
+      var message = '@Δ\x200¡P¿p£_!1AQaq$Φ"2BRbr¥Γ#3CScsèΛ¤4DTdtéΩ%5EUeuùΠ&6FVfvìΨ\'7GWgwòΣ(8HXhxÇΘ)9IYiy\nΞ*:JZjzØ+;KÄkäøÆ,<LÖlö\ræ-=MÑmñÅß.>NÜnüåÉ/?O§oà|^€{}[~]\\\f';
       var result = detector.DetectCharacterSet(message);
       assert.equal(result, 'GSM');
     });
