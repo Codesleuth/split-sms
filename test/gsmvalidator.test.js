964eaec8dc00d317b294389022d3704ed8fc4f6a
David Wood <david.p.wood@gmail.com>
2014-08-23 22:31:17 +0100

    Alters GSM validator to index a sparsely indexed associative array for improved performance.

--- test/gsmvalidator.test.js
+++ test/gsmvalidator.test.js
@@ -3,9 +3,9 @@ var assert = require('assert'),
 
 describe('GSM Validator', function () {
 
-  describe('Validating a message of all the known GSM characters', function () {
+  describe('Validating a message of every valid GSM characters', function () {
 
-    it('should return GSM', function () {
+    it('should return true', function () {
       var message = '@Δ\x200¡P¿p£_!1AQaq$Φ"2BRbr¥Γ#3CScsèΛ¤4DTdtéΩ%5EUeuùΠ&6FVfvìΨ\'7GWgwòΣ(8HXhxÇΘ)9IYiy\nΞ*:JZjzØ+;KÄkäøÆ,<LÖlö\ræ-=MÑmñÅß.>NÜnüåÉ/?O§oà|^€{}[~]\\\f';
       var result = gsmValidator.validateMessage(message);
       assert.strictEqual(result, true);
@@ -15,10 +15,12 @@ describe('GSM Validator', function () {
 
   describe('Validating a message of one GSM character', function () {
 
-    it('should return GSM', function () {
-      var message = '@';
-      var result = gsmValidator.validateMessage(message);
-      assert.strictEqual(result, true);
+    it('should return true', function () {
+      var gsm = '@Δ\x200¡P¿p£_!1AQaq$Φ"2BRbr¥Γ#3CScsèΛ¤4DTdtéΩ%5EUeuùΠ&6FVfvìΨ\'7GWgwòΣ(8HXhxÇΘ)9IYiy\nΞ*:JZjzØ+;KÄkäøÆ,<LÖlö\ræ-=MÑmñÅß.>NÜnüåÉ/?O§oà|^€{}[~]\\\f';
+      for (var i = 0; i < gsm.length; i++) {
+        var result = gsmValidator.validateMessage(gsm[i]);
+        assert.strictEqual(result, true, 'checking character "' + gsm[i] + '"');
+      }
     });
 
   });
@@ -26,8 +28,7 @@ describe('GSM Validator', function () {
   describe('Validating a message of one non-GSM characters', function () {
 
     it('should return false', function () {
-      var message = '\u1F433';
-      var result = gsmValidator.validateMessage(message);
+      var result = gsmValidator.validateMessage('\u1F433');
       assert.strictEqual(result, false);
     });
 
@@ -37,23 +38,28 @@ describe('GSM Validator', function () {
 
     var gsm = '@Δ\x200¡P¿p£_!1AQaq$Φ"2BRbr¥Γ#3CScsèΛ¤4DTdtéΩ%5EUeuùΠ&6FVfvìΨ\'7GWgwòΣ(8HXhxÇΘ)9IYiy\nΞ*:JZjzØ+;KÄkäøÆ,<LÖlö\ræ-=MÑmñÅß.>NÜnüåÉ/?O§oà|^€{}[~]\\\f';
 
-    for (var i = 0; i < gsm.length; i++) {
-      (function (char) {
-        it('should return true', function () {
-          var message = '\u1F433';
-          var result = gsmValidator.validateCharacter(char);
-          assert.strictEqual(result, true);
-        });
-      })(gsm[i]);
-    }
+    it('should return true', function () {
+      for (var i = 0; i < gsm.length; i++) {
+        var result = gsmValidator.validateCharacter(gsm[i]);
+        assert.strictEqual(result, true, 'checking character "' + gsm[i] + '"');
+      }
+    });
 
   });
 
   describe('Validating a non-GSM character', function () {
 
     it('should return false', function () {
-      var char = '\u1F433';
-      var result = gsmValidator.validateCharacter(char);
+      var result = gsmValidator.validateCharacter('\u1F433');
+      assert.strictEqual(result, false);
+    });
+
+  });
+
+  describe('Validating a GSM character with more than one character', function () {
+
+    it('should return false', function () {
+      var result = gsmValidator.validateCharacter('EUe');
       assert.strictEqual(result, false);
     });
 
