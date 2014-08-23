ad9b723ad3f994ea9ed72c53a2b81f49a97b28a2
David Wood <david.p.wood@gmail.com>
2014-08-23 21:47:26 +0100

    Enables message summary mode and adds more summary data.

--- test/splitter.test.js
+++ test/splitter.test.js
@@ -14,25 +14,59 @@ function testMessage(testData, splitterFunction) {
       result = splitterFunction(testData.message);
     });
 
-    it('should be ' + testData.parts.length + ' parts', function () {
-      assert.equal(result.length, testData.parts.length);
+    it('should have ' + testData.parts.length + ' parts', function () {
+      assert.strictEqual(result.parts.length, testData.parts.length);
+    });
+
+    it('should have ' + testData.totalLength + ' total characters', function () {
+      assert.strictEqual(result.totalLength, testData.totalLength);
+    });
+
+    it('should have ' + testData.totalBytes + ' total bytes', function () {
+      assert.strictEqual(result.totalBytes, testData.totalBytes);
     });
 
     for (var i = 0; i < testData.parts.length; i++) {
-      (function (part, index) {
-        it('should have the expected content in part ' + (index + 1), function () {
-          assert.equal(result[index].part, part.message);
+      (function (i, expected) {
+        it('should have the expected content in part ' + (i + 1), function () {
+          assert.strictEqual(result.parts[i].content, expected.content);
         });
 
-        it('should have the expected length in part ' + (index + 1), function () {
-          assert.equal(result[index].length, part.length);
+        it('should have the expected length in part ' + (i + 1), function () {
+          assert.strictEqual(result.parts[i].length, expected.length);
         });
 
-        it('should have the expected bytes in part ' + (index + 1), function () {
-          assert.equal(result[index].bytes, part.bytes);
+        it('should have the expected bytes in part ' + (i + 1), function () {
+          assert.strictEqual(result.parts[i].bytes, expected.bytes);
         });
-      })(testData.parts[i], i);
+      })(i, testData.parts[i]);
     }
+
+  });
+
+}
+
+function testMessageSummary(testData, splitterFunction) {
+
+  describe(testData.description, function () {
+
+    var result;
+
+    before(function () {
+      result = splitterFunction(testData.message, { summary: true });
+    });
+
+    it('should have ' + testData.parts.length + ' parts', function () {
+      assert.strictEqual(result.parts, testData.parts.length);
+    });
+
+    it('should have ' + testData.totalLength + ' total characters', function () {
+      assert.strictEqual(result.totalLength, testData.totalLength);
+    });
+
+    it('should have ' + testData.totalBytes + ' total bytes', function () {
+      assert.strictEqual(result.totalBytes, testData.totalBytes);
+    });
 
   });
 
@@ -48,10 +82,26 @@ describe('SMS Splitter', function () {
 
   });
 
+  describe('GSM Message Summary', function () {
+
+    for (var i = 0; i < gsmTestData.length; i++) {
+      testMessageSummary(gsmTestData[i], gsmSplitter.split);
+    }
+
+  });
+
   describe('Unicode Message', function () {
 
     for (var i = 0; i < unicodeTestData.length; i++) {
       testMessage(unicodeTestData[i], unicodeSplitter.split);
+    }
+
+  });
+
+  describe('Unicode Message Summary', function () {
+
+    for (var i = 0; i < unicodeTestData.length; i++) {
+      testMessageSummary(unicodeTestData[i], unicodeSplitter.split);
     }
 
   });
